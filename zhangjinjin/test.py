13a067697fae91468e23852dbe35532fa452b4c6
zj <798096606@qq.com>
2019-10-11 18:19:32 +0800

    增加提交报告弹窗处理

---
+++ zhangjinjin/test.py
@@ -0,0 +1,25 @@
+class Employee:
+    """所有员工的基类"""
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print("Name : ", self.name, ",Salary: ", self.salary)
+
+
+emp1 = Employee("Zara", 2000)
+emp2 = Employee("Zara", 2000)
+emp1.displayCount()
+emp2.displayCount()
+emp1.displayEmployee()
+emp2.displayEmployee()
+
+print(Employee.__module__)
+
